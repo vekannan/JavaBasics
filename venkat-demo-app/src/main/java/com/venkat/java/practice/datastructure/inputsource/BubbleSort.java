f44e1d89f09052531b04601dd2655b1bbceff161
U-CORP\vekannan <vekannan@D-SJC-00531088.corp.ebay.com>
2015-08-22 16:58:24 -0700

    First update in git Bubble Sorting algorithm

---
+++ venkat-demo-app/src/main/java/com/venkat/java/practice/datastructure/inputsource/BubbleSort.java
@@ -0,0 +1,50 @@
+package com.venkat.java.practice.datastructure.inputsource;
+
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.venkat.java.practice.datastructure.source.SortingInputSource;
+
+public class BubbleSort {
+	private static int[] arrayToSort;
+	private static int swapCount;
+	private static final Logger LOGGER = Logger.getLogger(BubbleSort.class.getName());
+
+	public static BubbleSort getInstatnce(int[] sortArray){
+		return new BubbleSort(sortArray); 
+	}
+	
+	public static void main(String[] args) {
+		BubbleSort.getInstatnce(SortingInputSource.getDefultSizeUnsortedIntArray());
+		bubbleSort();
+		LOGGER.log(Level.INFO, "Sorted Array ==> " + Arrays.toString(arrayToSort) + "from " +swapCount +" swaps");
+	}
+	
+	public static int[] bubblesort(){
+		bubbleSort();
+		LOGGER.log(Level.INFO, "Sorted Array ==> " + Arrays.toString(arrayToSort) + "from " +swapCount +" swaps");
+		return arrayToSort;
+	}
+	
+	private BubbleSort(int[] sortArray){
+		arrayToSort = sortArray;
+	}
+	
+	private static void bubbleSort(){
+		for(int i = 0; i < arrayToSort.length; i++){
+			for(int j = 0 ; j < arrayToSort.length-1 ; j++ ){
+				int k = j+1;
+				swapCount += ((arrayToSort[j] > arrayToSort[k] ) ? swapArray(j, k) : 0 );
+			}
+		}
+	}
+	
+	private static int swapArray(int i , int j){
+		int temp = arrayToSort[i] ;
+		arrayToSort[i] = arrayToSort[j];
+		arrayToSort[j] = temp;
+		return 1;
+	}
+
+}
---
+++ venkat-demo-app/src/main/java/com/venkat/java/practice/datastructure/source/SortingInputSource.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.venkat.java.practice.datastructure.source;
+
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author vekannan
+ *
+ */
+public class SortingInputSource {
+	private static final Logger LOGGER = Logger.getLogger(SortingInputSource.class.getName());
+	static int[] unsortedRandomArray;
+	final static int defaultArraySize = 10;
+	
+	public static int[] getDefultSizeUnsortedIntArray(){
+		LOGGER.log(Level.INFO, "Creating defaultsize(10) array of type int");
+		unsortedRandomArray = new int[10];
+		for(int i = 0; i < defaultArraySize; i++ ){
+			unsortedRandomArray[i] =  (int)(Math.random()*100);
+		}
+		LOGGER.log(Level.INFO, "Unsorted Array ==>"+Arrays.toString(unsortedRandomArray));
+		return unsortedRandomArray;
+	}
+
+}
